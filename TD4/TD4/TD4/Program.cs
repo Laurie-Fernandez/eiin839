d9917a2c1f67eb3fc2a8a846e5323a8b8f1c3b26
Laurie-Fernandez <59823387+Laurie-Fernandez@users.noreply.github.com>
2022-03-11 10:28:54 +0100

    TD4

---
+++ TD4/TD4/TD4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TD4.ServiceReference1;
+
+namespace TD4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorSoapClient csclient = new CalculatorSoapClient();
+            Console.WriteLine(csclient.Add(2, 5));
+            Console.ReadLine();
+        }
+    }
+}
